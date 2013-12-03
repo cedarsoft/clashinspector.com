b285e93c778431419cd90660eb6d57e457740004
Johannes Schneider <js@cedarsoft.com>
2013-12-03 21:53:41 +0100

    reformatted

--- src/test/java/com.cedarsoft.hsrt.zkb.ourplugin/DependencyServiceTest.java
+++ src/test/java/com.cedarsoft.hsrt.zkb.ourplugin/DependencyServiceTest.java
@@ -35,14 +35,13 @@ public class DependencyServiceTest {
   //Nicht vergessen, dass die scopes etc. mitgegeben werden sollen
 
   @Before
-  public void init()
-  {
+  public void init() {
     this.repoSystem = ManualRepositorySystemFactory.newRepositorySystem();
 
     this.repoSession = MavenRepositorySystemUtils.newSession();
 
     LocalRepository localRepo = new LocalRepository( "C:\\Users\\m\\.m2\\repository" );
-    this.repoSession.setLocalRepositoryManager( this.repoSystem.newLocalRepositoryManager(  this.repoSession, localRepo ) );
+    this.repoSession.setLocalRepositoryManager( this.repoSystem.newLocalRepositoryManager( this.repoSession, localRepo ) );
 
     //session.setTransferListener( new ConsoleTransferListener() );
     // session.setRepositoryListener( new ConsoleRepositoryListener() );
@@ -50,26 +49,23 @@ public class DependencyServiceTest {
   }
 
   @Test
-  public void testTestprojekt1()
-        {
+  public void testTestprojekt1() {
 
-          //Artifact artifact = new DefaultArtifact( "com.cedarsoft.hsrt.zkb:ourplugin-maven-plugin:maven-plugin:0.1-SNAPSHOT" );
-          Artifact artifact = new DefaultArtifact( "Testprojekt1:Testprojekt1_A:1.0-SNAPSHOT" );
-         CollectResult collectResult = dependencyService.getDependencyTree( artifact, repoSession, repoSystem, false );
+    //Artifact artifact = new DefaultArtifact( "com.cedarsoft.hsrt.zkb:ourplugin-maven-plugin:maven-plugin:0.1-SNAPSHOT" );
+    Artifact artifact = new DefaultArtifact( "Testprojekt1:Testprojekt1_A:1.0-SNAPSHOT" );
+    CollectResult collectResult = dependencyService.getDependencyTree( artifact, repoSession, repoSystem, false );
 
-          ClashCollectResultWrapper clashCollectResultWrapper = new ClashCollectResultWrapper( collectResult );
-
-
-          assertEquals(0,clashCollectResultWrapper.getNumberOfOuterClashes());
-          assertEquals(2,clashCollectResultWrapper.getNumberOfTotalDependencies());
+    ClashCollectResultWrapper clashCollectResultWrapper = new ClashCollectResultWrapper( collectResult );
 
 
+    assertEquals( 0, clashCollectResultWrapper.getNumberOfOuterClashes() );
+    assertEquals( 2, clashCollectResultWrapper.getNumberOfTotalDependencies() );
 
-        }
+
+  }
 
   @Test
-  public void testTestprojekt2()
-  {
+  public void testTestprojekt2() {
 
     //Artifact artifact = new DefaultArtifact( "com.cedarsoft.hsrt.zkb:ourplugin-maven-plugin:maven-plugin:0.1-SNAPSHOT" );
     Artifact artifact = new DefaultArtifact( "Testprojekt2:Testprojekt2_A:1.0-SNAPSHOT" );
@@ -78,26 +74,23 @@ public class DependencyServiceTest {
     ClashCollectResultWrapper clashCollectResultWrapper = new ClashCollectResultWrapper( collectResult );
 
 
-
     //Anzahl aller Dependencies
-    assertEquals("Wrong number of total dependencies",3,clashCollectResultWrapper.getNumberOfTotalDependencies());
+    assertEquals( "Wrong number of total dependencies", 3, clashCollectResultWrapper.getNumberOfTotalDependencies() );
 
     //Anzahl der Clashes auf Projektebene
-    assertEquals("Wrong number of total project clashes",1,clashCollectResultWrapper.getNumberOfOuterClashes());
+    assertEquals( "Wrong number of total project clashes", 1, clashCollectResultWrapper.getNumberOfOuterClashes() );
 
     //Anzahl der Clashes für eine bestimmte ClashSeverity
-    assertEquals("Number of Clashes with Severity Safe wrong.",0,clashCollectResultWrapper.getNumberOfOuterClashes( ClashSeverity.SAFE));
-    assertEquals("Number of Clashes with Severity Unsafe wrong.",0,clashCollectResultWrapper.getNumberOfOuterClashes( ClashSeverity.UNSAFE));
-    assertEquals("Number of Clashes with Severity Critical wrong.",1,clashCollectResultWrapper.getNumberOfOuterClashes( ClashSeverity.CRITICAL));
+    assertEquals( "Number of Clashes with Severity Safe wrong.", 0, clashCollectResultWrapper.getNumberOfOuterClashes( ClashSeverity.SAFE ) );
+    assertEquals( "Number of Clashes with Severity Unsafe wrong.", 0, clashCollectResultWrapper.getNumberOfOuterClashes( ClashSeverity.UNSAFE ) );
+    assertEquals( "Number of Clashes with Severity Critical wrong.", 1, clashCollectResultWrapper.getNumberOfOuterClashes( ClashSeverity.CRITICAL ) );
 
     //Anzahl der Clashes für eine bestimmte ClashSeverityLevel
-    assertEquals("Number of Clashes with Severity for Safe wrong.",1,clashCollectResultWrapper.getNumberOfOuterClashesForSeverityLevel( ClashSeverity.SAFE));
-    assertEquals("Number of Clashes with Severity for Unsafe wrong.",1,clashCollectResultWrapper.getNumberOfOuterClashesForSeverityLevel( ClashSeverity.UNSAFE));
-    assertEquals("Number of Clashes with Severity for Critical wrong.",1,clashCollectResultWrapper.getNumberOfOuterClashesForSeverityLevel( ClashSeverity.CRITICAL));
+    assertEquals( "Number of Clashes with Severity for Safe wrong.", 1, clashCollectResultWrapper.getNumberOfOuterClashesForSeverityLevel( ClashSeverity.SAFE ) );
+    assertEquals( "Number of Clashes with Severity for Unsafe wrong.", 1, clashCollectResultWrapper.getNumberOfOuterClashesForSeverityLevel( ClashSeverity.UNSAFE ) );
+    assertEquals( "Number of Clashes with Severity for Critical wrong.", 1, clashCollectResultWrapper.getNumberOfOuterClashesForSeverityLevel( ClashSeverity.CRITICAL ) );
 
   }
-
-
 
 
 }

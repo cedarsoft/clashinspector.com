9e468ce1a69a8946ab28cd8c77f10b279714d74b
Johannes Schneider <js@cedarsoft.com>
2013-12-07 17:12:40 +0100

    added install script for test projects

---
+++ Plugin_Test/installTestProjects.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+function failed {
+ echo "###################################"
+ echo "###################################"
+ echo "Failure!"
+ echo "###################################"
+ echo "###################################"
+
+ exit 1
+}
+
+function inner {
+  cd $1
+
+  files=(Testprojekt*)
+  for ((i=${#files[@]}-1; i>=0; i--));
+  do
+    dir=${files[$i]}
+    echo "    -- ${dir} --"
+
+    cd $dir
+#    mvn install -Dmaven.repo.local=${localRepo} || failed $1
+    mvn install -Dmaven.repo.local=${localRepo} > /dev/null || failed $1
+    cd ..
+
+  done
+
+  cd ..
+}
+
+
+localRepo=$1
+echo "Using local repository: <$localRepo>"
+
+
+for f in ./Testprojekt*
+do
+  echo "Installing <$f>"
+  inner $f
+done
+

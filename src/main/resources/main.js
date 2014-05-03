d512c81dc2c1a66ceb6a80d545614bc75fc763b8
mkampa <github@martinkampa.de>
2014-05-03 12:17:16 +0200

    .

--- src/main/resources/main.js
+++ src/main/resources/main.js
@@ -44,24 +44,24 @@ function doGet(url,callbackFunction)
 
 }
 
-function buildTree(data)
-{                alert("jo");
-       	alert(data.artifactId);
 
-
-    var html =    '<ul>' ;
+function buildTree(data, html)
+{
+   html = html + '<ul>';
 
                for(var i=0;i<data.children.length;i++){
                  html = html + drawDependency(data.children[i].groupId,data.children[i].artifactId,data.children[i].version);
+                 if(data.children[i].children.length==0)  {
+                    html = html + '</li>' ;
+                 }
+                 else{
+                    buildTree(data.children[i], html);
+                 }
+                 if(i == data.children.length-1) {
+                    html = html + '</ul>' ;
+                 }
                }
-
-            html = html + '</ul>' ;
-
-     $("#leftMain").html( html);
-
-
-
-
+   return html;
 }
 
 function drawDependency(groupId,artifactId,version)
@@ -73,7 +73,7 @@ function drawDependency(groupId,artifactId,version)
                                              <span class="artifactId" title="artifactId">'+artifactId+'</span>   \
                                              <hr>                                      \
                                              <span class="version" title="version">'+version+'</span>  \
-                                         </li>         ' ;
+                                                  ' ;
     }
 
     function drawMainDependency(data)
